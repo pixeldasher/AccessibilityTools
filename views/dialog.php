b53b066670c2a1e01dc41eba7764a04808da58a9
Phillip Weidinger <weidinger@hochwarth-it.de>
2025-07-03 07:59:34 +0200

    Pass down module for use in template and use declarative shadow dom for encapsulation

--- views/dialog.php
+++ views/dialog.php
@@ -8,70 +8,77 @@ use function ProcessWire\__;
 
 ?>
 
-<nav
-    id="accessibility-tools-nav"
-    class="a11y-container"
-    aria-label="<?= __('Inhalte zur Barrierefreiheit', 'AccessibilityTools'); ?>">
-    <button
-        type="button"
-        class="a11y-button"
-        commandfor="accessibility-tools"
-        command="show-modal"
-        title="<?= __('Optionen zur Barrierefreiheit anzeigen', 'AccessibilityTools'); ?>">
-        #
-    </button>
+<hit-a11y>
+    <template shadowrootmode="closed">
+        <link rel="stylesheet" nonce="proxy" href="<?= $config->urls($module) ?>styles/main.css">
+        <script type="module" nonce="proxy" src="<?= $config->urls($module) ?>scripts/main.js"></script>
 
-    <dialog
-        id="accessibility-tools"
-        class="a11y-dialog"
-        aria-labelledby="accessibility-tools-headline">
-        <header class="a11y-dialog-header">
-            <h2 id="accessibility-tools-headline">
-                <?= __('Optionen zur Barrierefreiheit', 'AccessibilityTools'); ?>
-            </h2>
+        <nav
+            id="accessibility-tools-nav"
+            class="a11y-container"
+            aria-label="<?= __('Inhalte zur Barrierefreiheit', 'AccessibilityTools'); ?>">
+            <button
+                type="button"
+                class="a11y-button"
+                commandfor="accessibility-tools"
+                command="show-modal"
+                title="<?= __('Optionen zur Barrierefreiheit anzeigen', 'AccessibilityTools'); ?>">
+                #
+            </button>
 
-            <form
-                method="dialog"
-                aria-label="<?= __('Aktionen', 'AccessibilityTools'); ?>">
-                <button
-                    type="submit"
-                    class="a11y-button"
-                    title="<?= __('Optionen zur Barrierefreiheit ausblenden', 'AccessibilityTools'); ?>">
-                    #
-                </button>
-            </form>
-        </header>
+            <dialog
+                id="accessibility-tools"
+                class="a11y-dialog"
+                aria-labelledby="accessibility-tools-headline">
+                <header class="a11y-dialog-header">
+                    <h2 id="accessibility-tools-headline">
+                        <?= __('Optionen zur Barrierefreiheit', 'AccessibilityTools'); ?>
+                    </h2>
 
-        <div class="a11y-dialog-content">
-            <?php if (!empty($enableContrastMode)): ?>
-                KONTRAST
-            <?php endif; ?>
-            <?php if (!empty($enableFontSizeAdjust)): ?>
-                SCHRIFT
-            <?php endif; ?>
-        </div>
+                    <form
+                        method="dialog"
+                        aria-label="<?= __('Aktionen', 'AccessibilityTools'); ?>">
+                        <button
+                            type="submit"
+                            class="a11y-button"
+                            title="<?= __('Optionen zur Barrierefreiheit ausblenden', 'AccessibilityTools'); ?>">
+                            #
+                        </button>
+                    </form>
+                </header>
 
-        <footer>
-            <nav aria-labelledby="accessibility-tools-footer-headline">
-                <h3 id="accessibility-tools-footer-headline">Inhalte in zugänglicher Form:</h3>
-
-                <ul role="list">
-                    <?php if (!empty($easyLanguageUrl)): ?>
-                        <li>
-                            <a href="<?= $easyLanguageUrl; ?>">
-                                Einfacher Sprache
-                            </a>
-                        </li>
+                <div class="a11y-dialog-content">
+                    <?php if (!empty($enableContrastMode)): ?>
+                        KONTRAST
                     <?php endif; ?>
-                    <?php if (!empty($signLanguageUrl)): ?>
-                        <li>
-                            <a href="<?= $signLanguageUrl; ?>">
-                                Inhalte in Gebärdensprache
-                            </a>
-                        </li>
+                    <?php if (!empty($enableFontSizeAdjust)): ?>
+                        SCHRIFT
                     <?php endif; ?>
-                </ul>
-            </nav>
-        </footer>
-    </dialog>
-</nav>
+                </div>
+
+                <footer>
+                    <nav aria-labelledby="accessibility-tools-footer-headline">
+                        <h3 id="accessibility-tools-footer-headline">Inhalte in zugänglicher Form:</h3>
+
+                        <ul role="list">
+                            <?php if (!empty($easyLanguageUrl)): ?>
+                                <li>
+                                    <a href="<?= $easyLanguageUrl; ?>">
+                                        Einfacher Sprache
+                                    </a>
+                                </li>
+                            <?php endif; ?>
+                            <?php if (!empty($signLanguageUrl)): ?>
+                                <li>
+                                    <a href="<?= $signLanguageUrl; ?>">
+                                        Inhalte in Gebärdensprache
+                                    </a>
+                                </li>
+                            <?php endif; ?>
+                        </ul>
+                    </nav>
+                </footer>
+            </dialog>
+        </nav>
+    </template>
+</hit-a11y>
